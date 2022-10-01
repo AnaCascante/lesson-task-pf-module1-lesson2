2c233d70a97dce58321ad3a57cad4ed88d290158
AnaCascante <ana.cascante@stud.noroff.no>
2022-10-01 17:09:59 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//question 1 
+var integer= 10; 
+var decimal= 9.9;  
+
+//question 2
+var workday="manday-tuesday-wednesday-thursday-friday";
+
+//question 3 
+var result= 5 + 5;
+var result= 5 - 1; 
+var result= 10 * 2; 
+var result= 20 / 2;
+
+//question 4
+var onSpecial = false; 
+
+//question 5 
+
+var animal = "The frogs are angry";
+var lengthOfString = animal.length; 
+console.log (lengthOfString)
+
+
+//question 6
+var timeElipsed;  
+var totalPrice;
+
+//question 7
+var number1= "53";
+var convertedNumber1= parseint(integer); 
+var number2= "44.6";
+var convertedNumber2= parseFloat (decimalNumber);
+
+//question 8
+var pet= "donkey"; 
+var typeOfPet = typeof pet;
+console.log  (typeOfPet)
+
+var age= 89;
+var typeOfAge = typeof age; 
+console.log (typeOfAge)
+
+var discountApplied= true;
+var typeOfDiscountApplied = typeof discountApplied;
+console.log (typeOfDiscountApplied)
+var timeLeft; 
+var typeOfTimeLeft = typeof timeLeft; 
+console.log (typeOfTimeLeft)
+
+//question 9
+
+var h1 ="Programming Foundations Module 1 Lesson Task 2"
+
+//question 10
+
+
+
